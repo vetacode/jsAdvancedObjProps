8be919e1d0398b1181cf2c9bfd43466462845344
vetacode <vetacode@gmail.com>
2025-11-24 14:44:12 +0700

    getting and setting new props with get and set

--- propGettersSetters.js
+++ propGettersSetters.js
@@ -1,3 +1,30 @@
 //2 KIND OF OBJECT PROPERTIES: DATA PROPERTIES AND ACCESSOR PROPERTY
 
 //GETTERS AND SETTERS
+let obj = {
+  get propName() {
+    // getter, the code executed on getting obj.propName
+  },
+
+  set propName(value) {
+    // setter, the code executed on setting obj.propName = value
+  },
+};
+
+let user = {
+  firstName: 'Abi',
+  lastName: 'Abigail',
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  },
+
+  set fullName(value) {
+    [this.firstName, this.lastName] = value.split(' ');
+  },
+};
+
+console.log(user.fullName); // Abi Abigail
+
+user.fullName = 'Alice Wonder';
+console.log(user.firstName);
+console.log(user.lastName);
