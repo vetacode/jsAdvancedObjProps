67e0974da808e43c28a2ce9cca19180f0ec03d28
vetacode <vetacode@gmail.com>
2025-11-24 12:35:35 +0700

    adding defineProp

--- PropFlagsDescriptors.js
+++ PropFlagsDescriptors.js
@@ -1,1 +1,32 @@
+'use strict';
+
 //Property flags and descriptors
+//It returns property descriptors
+
+let user = {
+  name: 'John',
+};
+
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+
+console.log(JSON.stringify(descriptor, null, 2));
+/* property descriptor:
+{
+  "value": "John",
+  "writable": true,
+  "enumerable": true,
+  "configurable": true
+}
+*/
+//TO CHANGE FLAGS:
+//SYNTAX: Object.defineProperty(obj, propertyName, descriptor)
+let user2 = {
+  name: 'John',
+};
+
+Object.defineProperty(user2, 'name', {
+  writable: false,
+});
+
+// user2.name = 'Pete'; //ga ngaruh coz udh read-only
+console.log(user2); //tetap {name: 'John'}
